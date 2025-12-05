0a1e0b8eb42fbbe53f836f2e52eb823840047577
remenod <remenod@gmail.com>
2025-12-05 03:35:50 +0200

    fix `install.sh`

--- install.sh
+++ install.sh
@@ -3,13 +3,14 @@
 set -e
 
 INSTALL_DIR="/usr/bin"
+REPO="https://raw.githubusercontent.com/Remenod/path-utils/master/scripts"
 
-for script in scripts/*; do
-    if [ -f "$script" ]; then
-        sudo cp "$script" "$INSTALL_DIR/$(basename "$script")"
-        sudo chmod +x "$INSTALL_DIR/$(basename "$script")"
-        echo "Installed $(basename "$script")"
-    fi
+scripts=("pathadd" "pathdel" "pathls")
+
+for script in "${scripts[@]}"; do
+    sudo curl -fsSL "$REPO/$script" -o "$INSTALL_DIR/$script"
+    sudo chmod +x "$INSTALL_DIR/$script"
+    echo "Installed $script"
 done
 
 echo "Done."
